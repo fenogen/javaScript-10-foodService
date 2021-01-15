23adba78145e9b988bcc52aa83ae311c260906e4
Oleg Soldatenko <sunskays@gmail.com>
2021-01-15 15:48:16 +0200

    Переключатель темы и сохранение в пямяти браузера

--- src/index.js
+++ src/index.js
@@ -1,13 +1,57 @@
 import './styles.css';
-
 import menu from './menu.json'; 
 
 
-console.log('hello');
 console.table(menu);
-console.log(typeof menu);
 
-// console.log(menu.name);
+// -----------------------------------> Записали классы для удобства в переменные
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const { LIGHT, DARK } = Theme;
 
+// -----------------------------------> Получили ссылки
+const bodyRef = document.querySelector('body');
+const switchRef = document.querySelector('#theme-switch-toggle');
+
+// -----------------------------------> Установили условие темы из памяти браузера
+
+if (localStorage.getItem('key') === LIGHT || localStorage.getItem('key') === null) {
+    bodyRef.classList.add(LIGHT);
+    switchRef.checked = false;
+}
+
+if (localStorage.getItem('key') === DARK) {
+    bodyRef.classList.add(DARK);
+    switchRef.checked = true;
+}
+
+// -----------------------------------> Установили переключатель темы:
+
+const changeTheme = () => {
+    if (switchRef.checked === false) {
+        bodyRef.classList.replace(DARK, LIGHT);
+        fnMemory();
+    }
+    if (switchRef.checked === true) {
+        bodyRef.classList.replace(LIGHT, DARK)
+        fnMemory();
+    }
+    console.log('Change theme');
+}
+
+// -----------------------------------> Создали ф-ю сохранения имени класса в памяти
+const fnMemory = () => {
+    localStorage.setItem('key', bodyRef.className);
+    return;
+}
+
+switchRef.addEventListener('change', changeTheme);
+
+// let memoryClass;
+// memoryClass = bodyRef.className
+// localStorage.setItem('key', memoryClass);
+// console.log(localStorage.getItem('key'));
 
 // const obectCart = JSON.parse();
