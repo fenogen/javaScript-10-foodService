b29d598fcf6379c81664c89f160747eecf2a1a0f
Oleg Soldatenko <sunskays@gmail.com>
2021-01-15 17:22:39 +0200

    Сделан шаблон меню с помощью шаблонизатора Handlebars

--- src/index.js
+++ src/index.js
@@ -1,57 +1,17 @@
 import './styles.css';
+
+// ---------------------------> Сделали импорт кода по изменению темы
+import { bodyRef, switchRef } from './js/theme';
+
+// ----------------------------> Сделали импорт данных
 import menu from './menu.json'; 
-
-
 console.table(menu);
+// ----------------------------> Создали шабон разметки и импортировали его
+import ltemsTemplete from './template/list.hbs';
 
-// -----------------------------------> Записали классы для удобства в переменные
-const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
-};
-const { LIGHT, DARK } = Theme;
+// ----------------------------> Привязали наши данные к разметке
+const cardOfMenu = ltemsTemplete(menu);
 
-// -----------------------------------> Получили ссылки
-const bodyRef = document.querySelector('body');
-const switchRef = document.querySelector('#theme-switch-toggle');
-
-// -----------------------------------> Установили условие темы из памяти браузера
-
-if (localStorage.getItem('key') === LIGHT || localStorage.getItem('key') === null) {
-    bodyRef.classList.add(LIGHT);
-    switchRef.checked = false;
-}
-
-if (localStorage.getItem('key') === DARK) {
-    bodyRef.classList.add(DARK);
-    switchRef.checked = true;
-}
-
-// -----------------------------------> Установили переключатель темы:
-
-const changeTheme = () => {
-    if (switchRef.checked === false) {
-        bodyRef.classList.replace(DARK, LIGHT);
-        fnMemory();
-    }
-    if (switchRef.checked === true) {
-        bodyRef.classList.replace(LIGHT, DARK)
-        fnMemory();
-    }
-    console.log('Change theme');
-}
-
-// -----------------------------------> Создали ф-ю сохранения имени класса в памяти
-const fnMemory = () => {
-    localStorage.setItem('key', bodyRef.className);
-    return;
-}
-
-switchRef.addEventListener('change', changeTheme);
-
-// let memoryClass;
-// memoryClass = bodyRef.className
-// localStorage.setItem('key', memoryClass);
-// console.log(localStorage.getItem('key'));
-
-// const obectCart = JSON.parse();
+// ----------------------------> Привязали нашиу разметку к списку в HTML
+const listRef = document.querySelector('.js-menu');
+listRef.insertAdjacentHTML('beforeend', cardOfMenu);
