44a4b9911ebb98ac2133ecd12d23d33f0801d984
Oleg Soldatenko <sunskays@gmail.com>
2021-01-15 13:21:12 +0200

    import menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './styles.css';
+
+import menu from './menu.json'; 
+
+
+console.log('hello');
+console.table(menu);
+console.log(typeof menu);
+
+// console.log(menu.name);
+
+
+// const obectCart = JSON.parse();
